1e09efb78fc226fb022ae168ba8fb3fb1233ecf7
Hisham Haidar <hisham.haidar2018@hotmail.com>
2023-11-06 13:46:54 +0100

    "SW-16 && SW-14"  getting data as pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Alert, Typography } from "antd";
+import React, { useState } from "react";
+import Pages from "./Pages";
 
 function Home() {
+  const [peopleData, setPeopleData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [currPage, setCurrPage] = useState(1);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Typography.Title id="starwarH1">Star Wars</Typography.Title>
+      {error && <Alert type="error" showIcon closable message={error} />}
+      <Pages
+        currPage={currPage}
+        setCurrPage={setCurrPage}
+        peopleData={peopleData}
+        setPeopleData={setPeopleData}
+        setLoading={setLoading}
+        setError={setError}
+      />
+      <pre>{JSON.stringify(peopleData, 0, 2)}</pre>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
